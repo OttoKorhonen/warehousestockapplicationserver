2302c89d14565bd2c7ea61af2bbe2681476ca3f1
OttoKorhonen <otto@xd.fi>
2021-02-09 07:50:35 +0200

    fixing bugs

--- Server.js
+++ Server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const request = require('request');
+//const request = require('request');
 const cors = require('cors')
 const app = express();
 const fetch = require('node-fetch');
