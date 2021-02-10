3180ed1b4379ae0fa4636e5fb9285578f6ba805a
OttoKorhonen <otto@xd.fi>
2021-02-10 15:06:01 +0200

    getting ready for deployment

--- Server.js
+++ Server.js
@@ -88,6 +88,6 @@ app.get('https://warehousestockapplication/herokuapp.com/:category', (req, res) 
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
